45cf786001d7b384358900ecffbaadff606114e0
PollyIva <48523515+PollyIva@users.noreply.github.com>
2019-04-26 23:53:10 +0300

    task-1_4.cxx

--- assignment-3/task-1_4.cxx
+++ assignment-3/task-1_4.cxx
@@ -31,11 +31,76 @@ root→Key ≤ K, то узел K добавляется в правое под�
   3 1 4 2 | 3 1 4 2
 
 ****************************************************************************/
-
 #include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Node
+{
+   int Key;
+   Node *left,*right;
+};
+
+
+void add(int Key,Node *&MyTree)
+{
+
+	if (NULL==MyTree)
+    {
+		MyTree=new Node;
+		MyTree->Key=Key;
+		MyTree->left=MyTree->right=NULL;
+	}
+    if (Key<MyTree->Key)
+    {
+        if (MyTree->left!=NULL) add(Key,MyTree->left);
+        else
+        {
+            MyTree->left=new Node;
+            MyTree->left->left=MyTree->left->right=NULL;
+            MyTree->left->Key=Key;
+        }
+    }
+    if (Key>MyTree->Key)
+    {
+        if (MyTree->right!=NULL) add(Key,MyTree->right);
+        else
+        {
+        MyTree->right=new Node;
+        MyTree->right->left=MyTree->right->right=NULL;
+        MyTree->right->Key=Key;
+        }
+    }
+}
+
+void levelOrder(Node* root) {
+    queue <Node*> q;
+    if (root != nullptr) q.push(root);
+    while (!q.empty())
+    {
+        Node* node = q.front();
+        q.pop();
+        cout << node->Key << ' ';
+        if (node->left != nullptr)
+            q.push(node->left);
+        if (node->right != nullptr)
+            q.push(node->right);
+    }
+}
 
 int main()
 {
+    int n,x;
+    cin >> n;
+    Node* Tree = NULL;
+    for(int i=0;i<n;i++)
+    {
+        cin >> x;
+        add(x,Tree);
+    };
+    levelOrder(Tree);
     return 0;
 }
+
 
